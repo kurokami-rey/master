98b83917e37707b0437c5258f12b5149c118bdcf
Kurokami_rey <56182144+kurokami-rey@users.noreply.github.com>
2019-10-10 10:19:57 +0700

    Index.php
    Tugas

---
+++ Index.php
@@ -0,0 +1,15 @@
+<?php
+$a = 'muhammad reyka fathahillah' ;
+$b = 'rey' ;
+$c = 'X rpl 2' ;
+$d = '21' ;
+echo 'biodata' ;
+echo "<br>" ;
+echo 'nama ku '.$a ;
+echo "<br>" ;
+echo 'panggil aku '.$b ;
+echo "<br>" ;
+echo 'aku di kelas '.$c ;
+echo "<br>" ;
+echo 'aku absen'.$d ;
+?>
